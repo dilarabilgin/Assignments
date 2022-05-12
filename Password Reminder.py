f0acd94631493fb91a1b2e925f08ee1e4542da5c
Irem Dilara Bilgin <100362305+dilarabilgin@users.noreply.github.com>
2022-05-13 00:22:18 +0200

    Create Password Reminder.py

---
+++ Password Reminder.py
@@ -0,0 +1,7 @@
+myname = "Dilara"
+name = str(input("Please enter your name: "))
+
+if myname == name:
+  print("Hello Dilara! The password is: 2000")
+else:
+  print("Hello,", name, "!See you later.")
